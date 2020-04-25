e428a57224e655487ca5278ca22b7dbb10429bab
rutevisi <teamdev.rutevisi@gmail.com>
2020-04-25 16:20:06 +0700

    Commit selanjutnya

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
+
+require('dotenv').config(); 
 
 const app = express();
 
-const mongoURI = 'mongodb+srv://errbint:visi231rute@cluster-rutevisi-maqxl.mongodb.net/test?retryWrites=true&w=majority';
+app.use(cors());
+app.use(express.json());
 
+//DB CONNECTION
+const mongoURI = process.env.ATLAS_URI;
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(()=> console.log('Database terhubung, gan'))
-        .catch((err)=> console.log(err));
+        .catch((err)=> console.log(`DB Connection Error: ${err.message}`));
 
+//ROUTING address
 app.get('/', (req, res) => {
     res.send('Assalamualaikum dunia waalaikumsalam');
 })
 
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
+//PORT SERVER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server mengudara... ketinggian: ${PORT}`))
