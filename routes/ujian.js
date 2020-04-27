dda4bfe59644188f64752bd632168941e9471dce
R Bintang Bagus <rbintang.bagus11@gmail.com>
2020-04-27 15:23:35 +0700

    /api/ujian/mbti siap pake tinggal bikin soal yang banyak

--- routes/ujian.js
+++ routes/ujian.js
@@ -1,24 +1,34 @@
 const router = require('express').Router();
 const JSON = require('flatted');
+var shuffle = require('shuffle-array');
 
 let MBTIQuestion = require('../models/mbti-question.model');
 
 router.route('/mbti').get(async (req, res) => {
-    let tempA = await MBTIQuestion.find({ indicator : "A" }).sort({}).limit(3);
-    let tempB = await MBTIQuestion.find({ indicator : "B" }).sort({}).limit(3);
-    let tempC = await MBTIQuestion.find({ indicator : "C" }).sort({}).limit(3);
-    let tempD = await MBTIQuestion.find({ indicator : "D" }).sort({}).limit(3);
-    let tempE = await MBTIQuestion.find({ indicator : "E" }).sort({}).limit(3);
+    let tempA = await MBTIQuestion.find({ indicator : "A" });
+    let tempB = await MBTIQuestion.find({ indicator : "B" });
+    let tempC = await MBTIQuestion.find({ indicator : "C" });
+    let tempD = await MBTIQuestion.find({ indicator : "D" });
+    let tempE = await MBTIQuestion.find({ indicator : "E" });
 
-    // let soalsoal = Object.assign(tempA, tempB, tempC, tempD, tempE);
+    function shuffleAndSlice(array, start, end) {
+        temp = shuffle(array);
+        temp = temp.slice(start, end);
+        return temp;
+    }
+
+    tempA = shuffleAndSlice(tempA, 0, 3);
+    tempB = shuffleAndSlice(tempB, 0, 3);
+    tempC = shuffleAndSlice(tempC, 0, 3);
+    tempD = shuffleAndSlice(tempD, 0, 3);
+    tempE = shuffleAndSlice(tempE, 0, 3);
 
 
-    // const flatted = JSON.stringify(tempA);
+    
+    
+    var soalready = shuffle([...tempA,...tempB,...tempC,...tempD,...tempE]);
 
-    // const tempA =  await  MBTIQuestion.find({ indicator : "A" }).sort({}).limit(3);
-       
-
-    res.send([...tempA,...tempB,...tempC,...tempD,...tempE]);
+    res.send(soalready);
 
     //   .then(mbtiQuestions => res.json(mbtiQuestions))
     //   .catch(err => res.status(400).json(`Error: ${err}`));
